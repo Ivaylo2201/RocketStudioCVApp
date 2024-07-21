05f80c5ff8536aa12a48aa45731bce6b13bb5aef
Ivaylo Georgiev <120925473+Ivaylo2201@users.noreply.github.com>
2024-07-21 18:17:10 +0300

    swapped promise syntax for async/await

--- resources/views/components/form-page/script.blade.php
+++ resources/views/components/form-page/script.blade.php
@@ -6,68 +6,87 @@
     const universitiesSelect = document.getElementById("university");
     const technologiesSelect = document.getElementById("technologies");
 
+    const toggleVisibility = (elementId) => {
+        const element = document.getElementById(elementId);
+
+        const currentVisibility = element.style.visibility;
+        let toggledVisibility = null;
+
+        if (currentVisibility === "hidden")
+            toggledVisibility = "visible";
+        else
+            toggledVisibility = "hidden";
+
+        element.style.visibility = toggledVisibility;
+    };
+
+    const fetchData = async (url, body) => {
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-TOKEN": "{{ csrf_token() }}"
+                },
+                body: JSON.stringify(body),
+            });
+
+
+            if (!response.ok) {
+                throw new Error(`An error occurred`);
+            }
+
+            return await response.json();
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     addUniversity.addEventListener("click", () => {
-        const universityPopup = document.getElementById("add-university-popup");
-        universityPopup.style.visibility =
-            universityPopup.style.visibility === "hidden" ? "visible" : "hidden";
+        toggleVisibility("add-university-popup");
     });
 
     addTechnology.addEventListener("click", () => {
-        const technologyPopup = document.getElementById("add-technology-popup");
-        technologyPopup.style.visibility =
-            technologyPopup.style.visibility === "hidden" ? "visible" : "hidden";
+        toggleVisibility("add-technology-popup");
     });
 
-    addUniversityButton.addEventListener("click", () => {
+    addUniversityButton.addEventListener("click", async () => {
         const universityName = document.getElementById("university-name").value;
         const grade = document.getElementById("grade").value;
 
-        fetch("{{ route('add.university') }}", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "X-CSRF-TOKEN": "{{ csrf_token() }}",
-                },
-                body: JSON.stringify({
-                    name: universityName,
-                    grade: grade,
-                }),
-            })
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.created) {
-                    const option = document.createElement("option");
-                    option.value = data.university.name;
-                    option.textContent = data.university.name;
-                    option.setAttribute("grade", data.university.grade);
-                    universitiesSelect.appendChild(option);
-                }
-            })
-            .catch((err) => console.log(err));
+        const url = "{{ route('add.university') }}";
+        const body = {
+            name: universityName,
+            grade: grade
+        }
+
+        const data = await fetchData(url, body);
+
+        if (data && data.created) {
+            const option = document.createElement("option");
+            option.value = data.university.name;
+            option.textContent = data.university.name;
+            option.setAttribute("grade", data.university.grade);
+            universitiesSelect.appendChild(option);
+        }
     });
 
-    addTechnologyButton.addEventListener("click", () => {
+    addTechnologyButton.addEventListener("click", async () => {
         const technologyName = document.getElementById("technology-name").value;
 
-        fetch("{{ route('add.technology') }}", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "X-CSRF-TOKEN": "{{ csrf_token() }}",
-                },
-                body: JSON.stringify({
-                    name: technologyName,
-                }),
-            })
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.created) {
-                    const option = document.createElement("option");
-                    option.value = data.technology.name;
-                    option.textContent = data.technology.name;
-                    technologiesSelect.appendChild(option);
-                }
-            })
-            .catch((err) => console.log(err));
+        const url = "{{ route('add.technology') }}";
+        const body = {
+            name: technologyName
+        };
+
+        const data = await fetchData(url, body);
+
+        if (data && data.created) {
+            const option = document.createElement("option");
+            option.value = data.technology.name;
+            option.textContent = data.technology.name;
+            technologiesSelect.appendChild(option);
+        }
     });
 </script>
