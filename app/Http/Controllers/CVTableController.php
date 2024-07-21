4b66212e9618fc6d291323e68cd8933c6ac2f38c
Ivaylo Georgiev <120925473+Ivaylo2201@users.noreply.github.com>
2024-07-21 23:36:59 +0300

    Added more comments for complicated code

--- app/Http/Controllers/CVTableController.php
+++ app/Http/Controllers/CVTableController.php
@@ -12,8 +12,6 @@ class CVTableController extends Controller
 {
     private function aggregate(Carbon $start, Carbon $end)
     {
-        // Етикети за възрастовите групи
-        // и долна и горна граница за тхя
         $age_ranges = [
             '0-19' => [0, 19],
             '20-29' => [20, 29],
@@ -27,6 +25,9 @@ class CVTableController extends Controller
 
         // Проверяваме годините на всеки потребител свързан със св
         // и определяме към коя възрастова група принадлежи
+
+        // use -> Позволява на нестед функциите да използват неща
+        // дефинирани е скоупа на външната функция
         $cvs_grouped_by_age = $cvs->groupBy(function ($cv) use ($age_ranges) {
             $age = Carbon::parse($cv->person->date_of_birth)->age;
 
@@ -37,12 +38,53 @@ class CVTableController extends Controller
             }
         });
 
-        // Връщаме броят на потребителите за всяка възрастова група
-        // и взимаме всичките им умения (технологии) като ги филтрираме
-        // така, че да няма повтарящи се
+        // $cvs_grouped_by_age ще е колекция, която в JSON би изглеждала така:
+        // {
+        //     "0-19": [{<CV обект 1>}, {<CV обект 2>}],
+        //     "20-29": [{<CV обект 3>}, ...],
+        //     ...
+        // }
+
+        // mapWithKeys преобразува колекцията (отгоре) в следната:
+
+        // {
+        //     "0-19": [<брой кандидати>, <масив с уникални технологии от всички кандидати>],
+        //     "20-29": [<брой кандидати>, <масив с уникални технологии от всички кандидати>]
+        // }
+
         $age_range_data = $cvs_grouped_by_age->mapWithKeys(function ($group, $age_range) {
             $number_of_people = $group->count();
+
+            // 1. На всяка итерация взимаме cv ot group 
+            //    и връщаме всички имена на техн. свързани с него
+
+            // 2. След като сме обходили всичко ще имаме нещо такова:
+            //
+            //    $technologies = $group->flatMap([
+            //        ['python', 'php'],
+            //        ['c#', 'c++'],
+            //        ['python', 'c#']
+            //    ])
+
+            // 3. След това flatMap ще вземе този 2д масив и ще го
+            //    преобразува в едномерен:
+            //
+            //    $technologies = ['python', 'php', 'c#', 'c++', 'python', 'c#']
+
+            // 4. Прилагаме функцията unique на този вече 'плосък' масив,
+            //    която маха дублирани стойности, като оставя само първата срещната,
+            //    това означава, че масивът с който работим вече е:
+            //
+            //    $technologies = ['python', 'php', 'c#', 'c++']
+
+            // 5. Използваме ->values() за да реиндексираме колекцията след премахването от unique
+
             $technologies = $group->flatMap(function ($cv) {
+
+                // $cv->technologies->pluck('name') - Взима 'name' на всяка технология
+                // с която cv инстанцията е свързана в мапинг таблицата. Връща масив
+                // с 1 или повече елементи в случая стрингове (имената на технологиите)
+
                 return $cv->technologies->pluck('name');
             })->unique()->values();
 
@@ -55,7 +97,6 @@ class CVTableController extends Controller
         });
 
         return $age_range_data;
-
     }
 
     private function fetchUserCvs(Carbon $start, Carbon $end): Collection
